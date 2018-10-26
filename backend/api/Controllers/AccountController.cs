b7b23df3b8df31a4a9f241c425617a4e0847faf3
Lebedev Alexander <winden999@gmail.com>
2018-10-26 17:32:46 +0300

    finish registration for user

--- backend/api/Controllers/AccountController.cs
+++ backend/api/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using api.Models;
+using api.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace api.Controllers
 {
@@ -17,6 +20,33 @@ namespace api.Controllers
         public AccountController(UsersContext usersContext)
         {
             db = usersContext;
+        }
+
+        [HttpPost("registration")]
+        public async Task<IActionResult> Registration([FromBody] RegistrationUser data)
+        {
+            var now = DateTime.Now;
+            User user = new User { CreatedAt = now, UpdatedAt = now, Role = "User"};
+
+            PropertyInfo[] properties = typeof(RegistrationUser).GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                var value = property.GetValue(data);
+                var userProperty = user.GetType().GetProperty(property.Name);
+                if (userProperty != null)
+                {
+                    userProperty.SetValue(user, value);
+                }
+            }
+
+            PasswordHasher<User> passwordHasher = new PasswordHasher<User>();
+            string passwordHash = passwordHasher.HashPassword(user, user.Password);
+            user.Password = passwordHash;
+
+            db.Add(user);
+            await db.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", "Users", new { id = user.Id }, user);
         }
     }
 }
