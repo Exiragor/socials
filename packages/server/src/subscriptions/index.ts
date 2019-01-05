02246b0e368a83eb31bec451e0131eb699d93f40
Lebedev Alexander <winden999@gmail.com>
2019-01-05 16:17:35 +0300

    del

--- packages/server/src/subscriptions/index.ts
+++
@@ -1,9 +0,0 @@
-import { PubSub } from 'apollo-server-express'
-
-import * as MESSAGE_EVENTS from './message'
-
-export const EVENTS = {
-  MESSAGE: MESSAGE_EVENTS,
-};
-
-export default new PubSub();
--- packages/server/src/subscriptions/message.ts
+++
@@ -1,2 +0,0 @@
-export const CREATED = 'CREATED'
-export const UPDATED = 'UPDATED'
